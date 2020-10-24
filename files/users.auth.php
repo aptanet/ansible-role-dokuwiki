4a275a662237e9447d36fdd9da9819c7567a490c
Paul Tansom <paul@aptanet.com>
2020-10-25 00:25:02 +0100

    Create users.auth.php

---
+++ files/users.auth.php
@@ -0,0 +1,11 @@
+<?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,separated
+
+
+aptanet:$password-hash-here-pre-generated:Paul Tansom:paul@aptanet.com:admin,user
